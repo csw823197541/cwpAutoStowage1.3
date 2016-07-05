0dd7c50f5f3c7d77b509bb3f8e1b7ec44833ff71
csw823197541 <cx123456>
2016-07-05 17:14:25 +0800

    调bug

--- src/autoStow/CallAutoStow.java
+++ src/autoStow/CallAutoStow.java
@@ -11,7 +11,7 @@ public class CallAutoStow {
 //        System.loadLibrary("AutoStowToJava4");
 
         String filePath = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
-        System.load(filePath + "AutoStowToJava5.dll");
+        System.load(filePath + "AutoStowToJava6.dll");
     }
 
     public static native String callAutoStow(String str1, String str2, String str3, String str4);
--- src/mDispatcher/MDMoveDataChange.java
+++ src/mDispatcher/MDMoveDataChange.java
@@ -3,6 +3,7 @@ package mDispatcher;
 import importDataInfo.MoveInfo;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -14,7 +15,21 @@ public class MDMoveDataChange {
     public static List<MDMove> changeToMDMove(List<MoveInfo> moveInfoList) {
         List<MDMove> mdMoveList = new ArrayList<>();
 
+        //遍历传入的指令，转换成发箱服务所用的指令对象
+        for(MoveInfo moveInfo : moveInfoList) {
 
+            Long voyId = moveInfo.getVoyId();
+            String craneId = moveInfo.getBatchId();
+            Integer moveSeq = moveInfo.getMoveId();
+            Date startTime = moveInfo.getWorkingStartTime();
+            Date endTime = moveInfo.getWorkingEndTime();
+
+            List<MDBaseMove> moveList = new ArrayList<>();
+            MDBaseMove mdBaseMove = new MDBaseMove();
+
+            MDMove mdMove = new MDMove();
+
+        }
         return mdMoveList;
     }
 }
